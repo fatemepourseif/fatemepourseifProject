987aab69c1e6e8d54fb8e0a6e3de471f3dcbcc90
fatemepourseif <87670138+fatemepourseif@users.noreply.github.com>
2022-01-29 01:54:03 -0800

    Add files via upload

---
+++ components/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+    document.getElementById("mySidepanel").style.display="block";
+}
+  
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("mySidepanel").style.display="none";
+}
